6e388b87df7c685b77d9fc5cc023188416428d38
Charliexox <breakfastwindow@gmail.com>
2024-01-05 21:23:18 +1100

    NEW CONFIG
    CHANGED SETTINGS TO DESIRED

--- armips/include/config.s
+++ armips/include/config.s
@@ -11,7 +11,7 @@ FAIRY_TYPE_IMPLEMENTED equ 1
 
 // ALLOW_SAVE_CHANGES tells the assembler that it can build the code that pertains to expanding the save data for extra fields for our usage.
 // this is required for dex expansion and will break PKHeX compatibility!  comment this line out to prevent save changes from being made.
-.definelabel ALLOW_SAVE_CHANGES, 0
+.definelabel ALLOW_SAVE_CHANGES, 1
 
 // CRY_PSEUDOBANK_START defines the first pseudobank to be used as cries in the sdat.  cries are loaded differently to save on RAM space
 CRY_PSEUDOBANK_START equ 778
@@ -23,7 +23,7 @@ DEBUG_NEEDS_TESTING equ 0
 LEARNSET_TOTAL_MOVES equ 41 // 40+terminate - currently driven by gallade
 
 // BATTLE_MODE_FORCE_SET defines whether or not players will be able to switch out mons when the opponent sends out their next mon. The player will be able to choose themselves like normal if the following is 0, 1 if the player will be forced to use "set"
-BATTLE_MODE_FORCE_SET equ 0
+BATTLE_MODE_FORCE_SET equ 1
 
 // ALWAYS_HAVE_NATIONAL_DEX defines whether or not the player will always have the national dex unlocked.
 ALWAYS_HAVE_NATIONAL_DEX equ 0
@@ -35,10 +35,10 @@ ALWAYS_UNCAPPED_FRAME_RATE equ 0
 BATTLES_UNCAPPED_FRAME_RATE equ 0
 
 // FAST_TEXT_PRINTING will make the text print quickly, almost instantly
-FAST_TEXT_PRINTING equ 0
+FAST_TEXT_PRINTING equ 1
 
 //REUSABLE_TMS will make TMs infinite and hide the quantity number.
 REUSABLE_TMS equ 1
 
 //DELETABLE_HMS Allows HMs to be forgotten, this also makes their quantity reduce, but the infinite TMs change prevents this.
-DELETABLE_HMS equ 0
+DELETABLE_HMS equ 1
