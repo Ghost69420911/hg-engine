838b16f478098f098cb0f730645da7439bbe1788
Charliexox <breakfastwindow@gmail.com>
2024-09-26 21:32:11 +1000

    Palette update
    Added all 100 palettes

--- armips/asm/custom/tm_palettes.s
+++ armips/asm/custom/tm_palettes.s
@@ -1,7 +1,7 @@
 .nds
 .thumb
 
-// Aurora Crystal specific change
+// Thankyou Drayano!
 // Edits the palettes of modified TMs
 
 .open "base/arm9.bin", 0x02000000
@@ -29,171 +29,504 @@ PALETTE_BUG equ 610
 
 // Between each TM we need to skip 8 * (TM gap - 1) bytes
 
-// Change TM01 to Dark palette
-.skip 2
-.halfword PALETTE_DARK
-.skip 4
-
-// Change TM03 to Psychic palette
-.skip (8 * 1)
-.skip 2
-.halfword PALETTE_PSYCHIC
-.skip 4
-
-// Change TM05 to Dark palette
-.skip (8 * 1)
-.skip 2
-.halfword PALETTE_DARK
-.skip 4
-
-// Change TM09 to Poison palette
-.skip (8 * 3)
-.skip 2
-.halfword PALETTE_POISON
-.skip 4
-
-// Change TM21 to Psychic palette (Do not have Fairy)
-.skip (8 * 11)
-.skip 2
-.halfword PALETTE_PSYCHIC
-.skip 4
-
-// Change TM27 to Fighting palette
-.skip (8 * 5)
+// TM001 FOCUS_PUNCH
 .skip 2
 .halfword PALETTE_FIGHTING
 .skip 4
 
-// Change TM28 to Ground palette (was previously Bug)
+// TM002 DRAGON_CLAW
 .skip 2
-.halfword PALETTE_GROUND
+.halfword PALETTE_DRAGON
 .skip 4
 
-// Change TM32 to Bug palette
-.skip (8 * 3)
-.skip 2
-.halfword PALETTE_BUG
-.skip 4
-
-// Change TM34 to Poison palette
-.skip (8 * 1)
-.skip 2
-.halfword PALETTE_POISON
-.skip 4
-
-// Change TM43 to Fire palette
-.skip (8 * 8)
-.skip 2
-.halfword PALETTE_FIRE
-.skip 4
-
-// Change TM49 to Normal palette
-.skip (8 * 5)
-.skip 2
-.halfword PALETTE_NORMAL
-.skip 4
-
-// Change TM55 to Water palette
-.skip (8 * 5)
+// TM003 WATER_PULSE
 .skip 2
 .halfword PALETTE_WATER
 .skip 4
 
-// Change TM56 to Grass palette
-// Cancelled
+// TM004 CALM_MIND
 .skip 2
-.halfword PALETTE_DARK
+.halfword PALETTE_PSYCHIC
 .skip 4
 
-// Change TM58 to Fighting palette
-.skip (8 * 1)
+// TM005 ROAR
 .skip 2
-.halfword PALETTE_FIGHTING
+.halfword PALETTE_NORMAL
 .skip 4
 
-// Change TM62 to Flying palette
-.skip (8 * 3)
+// TM006 TOXIC
 .skip 2
-.halfword PALETTE_FLYING
+.halfword PALETTE_POISON
 .skip 4
 
-// Change TM63 to Dark palette
-.skip 2
-.halfword PALETTE_DARK
-.skip 4
-
-// Change TM67 to Steel palette
-.skip (8 * 3)
-.skip 2
-.halfword PALETTE_STEEL
-.skip 4
-
-// Change TM70 to Fire palette
-.skip (8 * 2)
-.skip 2
-.halfword PALETTE_FIRE
-.skip 4
-
-// Change TM72 to Ground palette
-.skip (8 * 1)
-.skip 2
-.halfword PALETTE_GROUND
-.skip 4
-
-// Change TM74 to Bug palette
-.skip (8 * 1)
-.skip 2
-.halfword PALETTE_BUG
-.skip 4
-
-// Change TM76 to Bug palette (cancelled)
-.skip (8 * 1)
-.skip 2
-.halfword PALETTE_ROCK
-.skip 4
-
-// Change TM77 to Electric palette
-.skip (8 * 0)
-.skip 2
-.halfword PALETTE_ELECTRIC
-.skip 4
-
-// Change TM78 to Ground palette
-.skip (8 * 0)
-.skip 2
-.halfword PALETTE_GROUND
-.skip 4
-
-// Change TM82 to Fire palette
-.skip (8 * 3)
-.skip 2
-.halfword PALETTE_FIRE
-.skip 4
-
-// Change TM83 to Normal palette
-.skip (8 * 0)
-.skip 2
-.skip 2 // No change needed here
-.skip 4
-
-/* TM85 is already Psychic */
-
-// Change TM87 to Ice palette
-.skip (8 * 3)
+// TM007 HAIL
 .skip 2
 .halfword PALETTE_ICE
 .skip 4
 
-// Change TM88 to Flying palette
-.skip (8 * 0)
+// TM008 BULK_UP
+.skip 2
+.halfword PALETTE_FIGHTING
+.skip 4
+
+// TM009 BULLET_SEED
+.skip 2
+.halfword PALETTE_GRASS
+.skip 4
+
+// TM010 HIDDEN_POWER
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM011 SUNNY_DAY
+.skip 2
+.halfword PALETTE_FIRE
+.skip 4
+
+// TM012 TAUNT
+.skip 2
+.halfword PALETTE_DARK
+.skip 4
+
+// TM013 ICE_BEAM
+.skip 2
+.halfword PALETTE_ICE
+.skip 4
+
+// TM014 BLIZZARD
+.skip 2
+.halfword PALETTE_ICE
+.skip 4
+
+// TM015 HYPER_BEAM
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM016 LIGHT_SCREEN
+.skip 2
+.halfword PALETTE_PSYCHIC
+.skip 4
+
+// TM017 PROTECT
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM018 RAIN_DANCE
+.skip 2
+.halfword PALETTE_WATER
+.skip 4
+
+// TM019 GIGA_DRAIN
+.skip 2
+.halfword PALETTE_GRASS
+.skip 4
+
+// TM020 SAFEGUARD
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM021 FRUSTRATION
+.skip 2
+.halfword PALETTE_DARK
+.skip 4
+
+// TM022 SOLARBEAM
+.skip 2
+.halfword PALETTE_GRASS
+.skip 4
+
+// TM023 IRON_TAIL
+.skip 2
+.halfword PALETTE_STEEL
+.skip 4
+
+// TM024 THUNDERBOLT
+.skip 2
+.halfword PALETTE_ELECTRIC
+.skip 4
+
+// TM025 THUNDER
+.skip 2
+.halfword PALETTE_ELECTRIC
+.skip 4
+
+// TM026 EARTHQUAKE
+.skip 2
+.halfword PALETTE_GROUND
+.skip 4
+
+// TM027 RETURN
+.skip 2
+.halfword PALETTE_PSYCHIC
+.skip 4
+
+// TM028 DIG
+.skip 2
+.halfword PALETTE_GROUND
+.skip 4
+
+// TM029 PSYCHIC
+.skip 2
+.halfword PALETTE_PSYCHIC
+.skip 4
+
+// TM030 SHADOW_BALL
+.skip 2
+.halfword PALETTE_GHOST
+.skip 4
+
+// TM031 BRICK_BREAK
+.skip 2
+.halfword PALETTE_FIGHTING
+.skip 4
+
+// TM032 DOUBLE_TEAM
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM033 REFLECT
+.skip 2
+.halfword PALETTE_PSYCHIC
+.skip 4
+
+// TM034 SHOCK_WAVE
+.skip 2
+.halfword PALETTE_ELECTRIC
+.skip 4
+
+// TM035 FLAMETHROWER
+.skip 2
+.halfword PALETTE_FIRE
+.skip 4
+
+// TM036 SLUDGE_BOMB
+.skip 2
+.halfword PALETTE_POISON
+.skip 4
+
+// TM037 SANDSTORM
+.skip 2
+.halfword PALETTE_ROCK
+.skip 4
+
+// TM038 FIRE_BLAST
+.skip 2
+.halfword PALETTE_FIRE
+.skip 4
+
+// TM039 ROCK_TOMB
+.skip 2
+.halfword PALETTE_ROCK
+.skip 4
+
+// TM040 AERIAL_ACE
 .skip 2
 .halfword PALETTE_FLYING
 .skip 4
 
-// Change TM90 to Dragon palette
-.skip (8 * 1)
+// TM041 TORMENT
+.skip 2
+.halfword PALETTE_DARK
+.skip 4
+
+// TM042 FACADE
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM043 SECRET_POWER
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM044 REST
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM045 ATTRACT
+.skip 2
+.halfword PALETTE_PSYCHIC
+.skip 4
+
+// TM046 THIEF
+.skip 2
+.halfword PALETTE_DARK
+.skip 4
+
+// TM047 STEEL_WING
+.skip 2
+.halfword PALETTE_STEEL
+.skip 4
+
+// TM048 SKILL_SWAP
+.skip 2
+.halfword PALETTE_PSYCHIC
+.skip 4
+
+// TM049 SNATCH
+.skip 2
+.halfword PALETTE_DARK
+.skip 4
+
+// TM050 OVERHEAT
+.skip 2
+.halfword PALETTE_FIRE
+.skip 4
+
+// TM051 ROOST
+.skip 2
+.halfword PALETTE_FLYING
+.skip 4
+
+// TM052 FOCUS_BLAST
+.skip 2
+.halfword PALETTE_FIGHTING
+.skip 4
+
+// TM053 ENERGY_BALL
+.skip 2
+.halfword PALETTE_GRASS
+.skip 4
+
+// TM054 FALSE_SWIPE
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM055 BRINE
+.skip 2
+.halfword PALETTE_WATER
+.skip 4
+
+// TM056 FLING
+.skip 2
+.halfword PALETTE_DARK
+.skip 4
+
+// TM057 CHARGE_BEAM
+.skip 2
+.halfword PALETTE_ELECTRIC
+.skip 4
+
+// TM058 ENDURE
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM059 DRAGON_PULSE
 .skip 2
 .halfword PALETTE_DRAGON
+.skip 4
+
+// TM060 DRAIN_PUNCH
+.skip 2
+.halfword PALETTE_FIGHTING
+.skip 4
+
+// TM061 WILL_O_WISP
+.skip 2
+.halfword PALETTE_FIRE
+.skip 4
+
+// TM062 SILVER_WIND
+.skip 2
+.halfword PALETTE_BUG
+.skip 4
+
+// TM063 EMBARGO
+.skip 2
+.halfword PALETTE_DARK
+.skip 4
+
+// TM064 EXPLOSION
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM065 SHADOW_CLAW
+.skip 2
+.halfword PALETTE_GHOST
+.skip 4
+
+// TM066 PAYBACK
+.skip 2
+.halfword PALETTE_DARK
+.skip 4
+
+// TM067 RECYCLE
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM068 GIGA_IMPACT
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM069 ROCK_POLISH
+.skip 2
+.halfword PALETTE_ROCK
+.skip 4
+
+// TM070 FLASH
+.skip 2
+.halfword PALETTE_ELECTRIC
+.skip 4
+
+// TM071 STONE_EDGE
+.skip 2
+.halfword PALETTE_ROCK
+.skip 4
+
+// TM072 AVALANCHE
+.skip 2
+.halfword PALETTE_ICE
+.skip 4
+
+// TM073 THUNDER_WAVE
+.skip 2
+.halfword PALETTE_ELECTRIC
+.skip 4
+
+// TM074 GYRO_BALL
+.skip 2
+.halfword PALETTE_STEEL
+.skip 4
+
+// TM075 SWORDS_DANCE
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM076 STEALTH_ROCK
+.skip 2
+.halfword PALETTE_ROCK
+.skip 4
+
+// TM077 PSYCH_UP
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM078 CAPTIVATE
+.skip 2
+.halfword PALETTE_PSYCHIC
+.skip 4
+
+// TM079 DARK_PULSE
+.skip 2
+.halfword PALETTE_DARK
+.skip 4
+
+// TM080 ROCK_SLIDE
+.skip 2
+.halfword PALETTE_ROCK
+.skip 4
+
+// TM081 X_SCISSOR
+.skip 2
+.halfword PALETTE_BUG
+.skip 4
+
+// TM082 SLEEP_TALK
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM083 NATURAL_GIST
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM084 POISON_JAB
+.skip 2
+.halfword PALETTE_POISON
+.skip 4
+
+// TM085 DREAM_EATER
+.skip 2
+.halfword PALETTE_PSYCHIC
+.skip 4
+
+// TM086 GRASS_KNOT
+.skip 2
+.halfword PALETTE_GRASS
+.skip 4
+
+// TM087 SWAGGER
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM088 PLUCK
+.skip 2
+.halfword PALETTE_FLYING
+.skip 4
+
+// TM089 U_TURN
+.skip 2
+.halfword PALETTE_BUG
+.skip 4
+
+// TM090 SUBSTITUTE
+.skip 2
+.halfword PALETTE_NORMAL
+.skip 4
+
+// TM091 FLASH_CANNON
+.skip 2
+.halfword PALETTE_STEEL
+.skip 4
+
+// TM092 TRICK_ROOM
+.skip 2
+.halfword PALETTE_PSYCHIC
+.skip 4
+
+// HM001 CUT
+.skip 2
+.halfword PALETTE_STEEL
+.skip 4
+
+// HM002 FLY
+.skip 2
+.halfword PALETTE_FLYING
+.skip 4
+
+// HM003 SURF
+.skip 2
+.halfword PALETTE_WATER
+.skip 4
+
+// HM004 STRENGTH
+.skip 2
+.halfword PALETTE_FIGHTING
+.skip 4
+
+// HM005 WHIRLPOOL
+.skip 2
+.halfword PALETTE_WATER
+.skip 4
+
+// HM006 ROCK_SMASH
+.skip 2
+.halfword PALETTE_FIGHTING
+.skip 4
+
+// HM007 WATERFALL
+.skip 2
+.halfword PALETTE_WATER
+.skip 4
+
+// HM008 ROCK_CLIMB
+.skip 2
+.halfword PALETTE_ROCK
 .skip 4
 
 .close
